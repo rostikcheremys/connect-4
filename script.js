32774b195eb43c53d985907246de754f52fe7af2
rostikcheremys <rostikcheremys@gmail.com>
2023-11-24 19:08:14 +0200

    finalization of the design

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-let player1 = "Player 1";
-let player2 = "Player 2";
-
-/*let player1 = prompt("Введіть ім'я першого грався:");
-let player2 = prompt("Введіть ім'я другого грався:");
-
-if (player1 === '') {
-    player1 = "Player 1";
-}
-
-if (player2 === '') {
-    player2 = "Player 2";
-}*/
-
 // Define the matrix to represent the game board
 let board = [
     [null, null, null, null, null, null],
@@ -28,65 +14,62 @@ let turn = 1;
 
 // Checking the winner
 function check(player) {
-    setTimeout(() => {
-        // Check for a winning condition using the 'board' matrix
-        for (let i = 0; i < 7; i++) {
-            for (let j = 0; j <= 2; j++) {
-                if (
-                    board[i][j] === player &&
-                    board[i][j + 1] === player &&
-                    board[i][j + 2] === player &&
-                    board[i][j + 3] === player
-                ) {
-                    alert(`${player} wins`);
-                    location.reload();
-                }
+    // Check for a winning condition using the 'board' matrix
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (
+                board[i][j] === player &&
+                board[i][j + 1] === player &&
+                board[i][j + 2] === player &&
+                board[i][j + 3] === player
+            ) {
+                document.getElementById("current-player").innerText = `${player} wins`;
+                location.reload();
             }
         }
+    }
 
-        for (let i = 0; i <= 5; i++) {
-            for (let j = 0; j <= 3; j++) {
-                if (
-                    board[j][i] === player &&
-                    board[j + 1][i] === player &&
-                    board[j + 2][i] === player &&
-                    board[j + 3][i] === player
-                ) {
-                    alert(`${player} wins`);
-                    location.reload();
-                }
+    for (let i = 0; i <= 5; i++) {
+        for (let j = 0; j <= 3; j++) {
+            if (
+                board[j][i] === player &&
+                board[j + 1][i] === player &&
+                board[j + 2][i] === player &&
+                board[j + 3][i] === player
+            ) {
+                document.getElementById("current-player").innerText = `${player} wins`;
+                location.reload();
             }
         }
+    }
 
-        for (let i = 0; i <= 3; i++) {
-            for (let j = 0; j <= 2; j++) {
-                if (
-                    board[i][j] === player &&
-                    board[i + 1][j + 1] === player &&
-                    board[i + 2][j + 2] === player &&
-                    board[i + 3][j + 3] === player
-                ) {
-                    alert(`${player} wins`);
-                    location.reload();
-                }
+    for (let i = 0; i <= 3; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (
+                board[i][j] === player &&
+                board[i + 1][j + 1] === player &&
+                board[i + 2][j + 2] === player &&
+                board[i + 3][j + 3] === player
+            ) {
+                document.getElementById("current-player").innerText = `${player} wins`;
+                location.reload();
             }
         }
+    }
 
-        for (let i = 0; i <= 3; i++) {
-            for (let j = 5; j >= 3; j--) {
-                if (
-                    board[i][j] === player &&
-                    board[i + 1][j - 1] === player &&
-                    board[i + 2][j - 2] === player &&
-                    board[i + 3][j - 3] === player
-                ) {
-                    alert(`${player} wins`);
-                    location.reload();
-                }
+    for (let i = 0; i <= 3; i++) {
+        for (let j = 5; j >= 3; j--) {
+            if (
+                board[i][j] === player &&
+                board[i + 1][j - 1] === player &&
+                board[i + 2][j - 2] === player &&
+                board[i + 3][j - 3] === player
+            ) {
+                document.getElementById("current-player").innerText = `${player} wins`;
+                location.reload();
             }
         }
-
-    }, 200);
+    }
 }
 
 // Playing
@@ -96,13 +79,13 @@ document.querySelectorAll(".column").forEach((e, columnIndex) => {
 
         if (row !== -1) {
             // Update the 'board' matrix
-            board[columnIndex][row] = turn % 2 !== 0 ? player1 : player2;
+            board[columnIndex][row] = turn % 2 !== 0 ? "Player 1" : "Player 2";
 
             // Update the visual representation
             document.getElementById(`${e.id}r${row + 1}`).classList.add(turn % 2 !== 0 ? 'red-chip' : 'yellow-chip');
 
             // Check for a winner
-            check(turn % 2 !== 0 ? player1 : player2);
+            check(turn % 2 !== 0 ? "Player 1" : "Player 2");
 
             // Switch turns
             turn++;
