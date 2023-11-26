c3d00191f4e579303ed262f330797c40cff08017
rostikcheremys <rostikcheremys@gmail.com>
2023-11-26 20:17:09 +0200

    completion of game development

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Define the matrix to represent the game board
 let board = [
     [null, null, null, null, null, null],
     [null, null, null, null, null, null],
@@ -9,89 +8,78 @@ let board = [
     [null, null, null, null, null, null]
 ];
 
-// Initialize other variables
 let turn = 1;
+let isGameWon = false;
+let isCheckWins = false;
 
-// Checking the winner
 function check(player) {
-    // Check for a winning condition using the 'board' matrix
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j <= 2; j++) {
-            if (
-                board[i][j] === player &&
-                board[i][j + 1] === player &&
-                board[i][j + 2] === player &&
-                board[i][j + 3] === player
-            ) {
-                document.getElementById("current-player").innerText = `${player} wins`;
-                location.reload();
+            if ( board[i][j] === player && board[i][j + 1] === player && board[i][j + 2] === player && board[i][j + 3] === player) {
+                isCheckWins = true;
             }
         }
     }
 
     for (let i = 0; i <= 5; i++) {
         for (let j = 0; j <= 3; j++) {
-            if (
-                board[j][i] === player &&
-                board[j + 1][i] === player &&
-                board[j + 2][i] === player &&
-                board[j + 3][i] === player
-            ) {
-                document.getElementById("current-player").innerText = `${player} wins`;
-                location.reload();
+            if (board[j][i] === player && board[j + 1][i] === player && board[j + 2][i] === player && board[j + 3][i] === player) {
+                isCheckWins = true;
             }
         }
     }
 
     for (let i = 0; i <= 3; i++) {
         for (let j = 0; j <= 2; j++) {
-            if (
-                board[i][j] === player &&
-                board[i + 1][j + 1] === player &&
-                board[i + 2][j + 2] === player &&
-                board[i + 3][j + 3] === player
-            ) {
-                document.getElementById("current-player").innerText = `${player} wins`;
-                location.reload();
+            if (board[i][j] === player && board[i + 1][j + 1] === player && board[i + 2][j + 2] === player && board[i + 3][j + 3] === player) {
+                isCheckWins = true;
             }
         }
     }
 
     for (let i = 0; i <= 3; i++) {
         for (let j = 5; j >= 3; j--) {
-            if (
-                board[i][j] === player &&
-                board[i + 1][j - 1] === player &&
-                board[i + 2][j - 2] === player &&
-                board[i + 3][j - 3] === player
-            ) {
-                document.getElementById("current-player").innerText = `${player} wins`;
-                location.reload();
+            if (board[i][j] === player && board[i + 1][j - 1] === player && board[i + 2][j - 2] === player && board[i + 3][j - 3] === player) {
+                isCheckWins = true;
             }
         }
     }
 }
 
-// Playing
-document.querySelectorAll(".column").forEach((e, columnIndex) => {
-    e.addEventListener("click", () => {
+function resetButton() {
+    document.getElementById('button').addEventListener('click', function() {
+        location.reload();
+    });
+}
+
+document.querySelectorAll(".column").forEach((element, columnIndex) => {
+    element.addEventListener("click", () => {
+        if (isGameWon) {
+            return resetButton();
+        }
+
         const row = board[columnIndex].indexOf(null);
 
-        if (row !== -1) {
-            // Update the 'board' matrix
-            board[columnIndex][row] = turn % 2 !== 0 ? "Player 1" : "Player 2";
+        if (row === -1) {
+            return;
+        }
 
-            // Update the visual representation
-            document.getElementById(`${e.id}r${row + 1}`).classList.add(turn % 2 !== 0 ? 'red-chip' : 'yellow-chip');
+        board[columnIndex][row] = turn % 2 !== 0 ? "Player 1" : "Player 2";
 
-            // Check for a winner
-            check(turn % 2 !== 0 ? "Player 1" : "Player 2");
+        document.getElementById(`${element.id}r${row + 1}`).classList.add(turn % 2 !== 0 ? 'red-chip' : 'yellow-chip');
 
-            // Switch turns
+        check(turn % 2 !== 0 ? "Player 1" : "Player 2");
+
+        if (board.every(column => column.every(cell => cell !== null))) {
+            document.getElementById('current-player').innerText = "The game ended in a draw. All positions are filled."
+        } else if (isCheckWins === false) {
             turn++;
-
-            // Update the turn indicator
-            document.getElementById("current-player").innerText = turn % 2 !== 0 ?  "Red's Turn" : "Yellow's Turn";
+            document.getElementById('current-player').innerText = turn % 2 !== 0 ?  "Red's Turn" : "Yellow's Turn";
+            resetButton();
+        } else {
+            isGameWon = true;
+            return document.getElementById('current-player').innerText = turn % 2 !== 0 ? "Reds Won!" : "Yellow Won!";
         }
     });
 });
+
