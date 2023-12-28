42e62daaafd002fbd939893fcb119cf3b8b53d32
rostikcheremys <rostikcheremys@gmail.com>
2023-12-28 14:18:55 +0200

    adding: sounds

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ let turn = 1;
 const columns = document.querySelectorAll(".column");
 const currentPlayer = document.getElementById('current-player');
 
+const audioTurn = new Audio("sounds/chip.mp3");
+const audioWon = new Audio("sounds/game-won.mp3");
+const audioDraw = new Audio("sounds/game-over.mp3");
+const audioButton = new Audio("sounds/button-click.mp3");
+
 const isRedTurn = () => turn % 2 !== 0;
 
 columns.forEach((element, index) => {
-    element.addEventListener('click', () => {
-        if (isGameWon) return resetButton();
+    element.addEventListener('click', async () => {
+        if (isGameWon) {
+            await audioWon.play();
+            return resetButton();
+        }
 
         const row = board[index].indexOf(null);
 
@@ -20,16 +28,19 @@ columns.forEach((element, index) => {
         board[index][row] = player;
         document.getElementById(`${element.id}r${row + 1}`).classList.add(`${player}-chip`);
 
-        updateGameStatus();
+        await audioTurn.play();
+        await updateGameStatus();
     });
 });
 
-function updateGameStatus() {
+async function updateGameStatus() {
     if (checkHorizontal() || checkVertical() || checkDiagonal()) {
         isGameWon = true;
+        await audioWon.play();
         currentPlayer.innerText = isRedTurn() ? "Reds Won!" : "Yellows Won!";
     } else if (board.every(column => column.every(cell => cell))) {
         currentPlayer.innerText = "The game ended in a draw. All positions are filled.";
+        await audioDraw.play();
     } else {
         turn++;
         currentPlayer.innerText = isRedTurn() ? "Red's Turn" : "Yellow's Turn";
@@ -38,7 +49,7 @@ function updateGameStatus() {
     resetButton();
 }
 
-function checkHorizontal() {
+function checkHorizontal () {
     for (let i = 0; i <= 6; i++) {
         for (let j = 0; j <= 2; j++) {
             if (isConnectFour(board[i][j], board[i][j + 1], board[i][j + 2], board[i][j + 3])) {
@@ -81,10 +92,17 @@ function isConnectFour(cell1, cell2, cell3, cell4) {
 }
 
 function resetButton() {
-    document.getElementById('button').addEventListener('click', function() {
+    document.getElementById('button').addEventListener('click', async function () {
+        await audioButton.play();
         this.classList.add('fade-in');
-        this.addEventListener('animationend', () => this.classList.remove('fade-in'), { once: true });
-        location.reload();
+        this.addEventListener('animationend', () => {
+            this.classList.remove('fade-in');
+            reloadPage();
+        }, { once: true });
     });
+}
+
+function reloadPage() {
+    location.reload();
 }
 
